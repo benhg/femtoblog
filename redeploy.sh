922431b304919ab438aafef48f548407dfca5eeb
Ben Glick <glick@lclark.edu>
2019-08-12 17:57:24 -0400

    fix redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -3,7 +3,9 @@
 if [ $# != 1 ]; then
                 echo "Usage: redeploy.sh <container id>"
         else
-            	sudo docker cp $1:/database.db project/database.db
+            	sudo docker cp $1:/app/database.db project/database.db
+            	sudo docker rm -f $1
+            	sudo docker cp rm $1
 				sudo docker build -t femtoblog project/
 				sudo docker run -d -p 80:8080 femtoblog
         fi
